eceaa5be5596f01d79f4836820782f4d95ebc74d
avdunitydev <avdunitydev@users.noreply.github.com>
2017-04-27 23:09:55 +0300

    Add script TextFade.cs

---
+++ Assets/Main_Scene/Scripts/TextFade.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour {
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.color = new Color (text.color.r, text.color.g, 
+		text.color.b, Mathf.PingPong(Time.time / 2.5f, 1f));
+	}
+}
