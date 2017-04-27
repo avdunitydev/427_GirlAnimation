17fe4560e4b83e34b6234802586e024217da1667
avdunitydev <avdunitydev@users.noreply.github.com>
2017-04-27 23:09:30 +0300

    Add script ScrolObject.cs

---
+++ Assets/Main_Scene/Scripts/ScrolObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrolObject : MonoBehaviour {
+	public float speed = 2f;
+	Transform girlPossition;
+
+	// Use this for initialization
+	void Start () {
+		girlPossition = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(girlPossition.position.x <= 0){
+			girlPossition.position += new Vector3(speed * Time.deltaTime, 
+			0, 0);
+		}
+	}
+}
