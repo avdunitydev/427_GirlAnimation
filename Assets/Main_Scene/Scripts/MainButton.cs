611cc6666d3f6d4bba8000419d4e69afec94bffb
avdunitydev <avdunitydev@users.noreply.github.com>
2017-04-27 23:08:38 +0300

    Add script MainButton.cs

---
+++ Assets/Main_Scene/Scripts/MainButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButton : MonoBehaviour 
+{
+	public float speed = 5f;
+	RectTransform rect;
+
+
+	void Start () 
+	{
+		rect = GetComponent<RectTransform>();
+	}
+
+	void Update () 
+	{
+		if (rect.offsetMin.y <= 0) 
+		{
+			rect.offsetMin += new Vector2 (rect.offsetMin.x, speed);
+		}
+	}
+	void OnMouseDown () {
+
+	}
+}
