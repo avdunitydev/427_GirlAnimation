7e159b591897a82d9239e1ea8eeed7685646e605
avdunitydev <avdunitydev@users.noreply.github.com>
2017-04-27 23:09:04 +0300

    Add script OutLineFade.cs

---
+++ Assets/Main_Scene/Scripts/OutLineFade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutLineFade : MonoBehaviour {
+	Outline oLine;
+
+	// Use this for initialization
+	void Start () {
+		oLine = GetComponent<Outline>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		oLine.effectColor = new Color(oLine.effectColor.r, 
+		oLine.effectColor.g, oLine.effectColor.b, 
+		Mathf.PingPong(Time.time, 1f));
+	}
+}
